72951a9c5e3070bbb0befb7507ef14cd295d8606
Neeti <neetikumar8@gmail.com>
2025-11-09 04:11:47 +0530

    plasma vfx clean up

--- Assets/Scripts/spherePositioning.cs
+++ Assets/Scripts/spherePositioning.cs
@@ -3,19 +3,10 @@ using UnityEngine;
 public class spherePositioning : MonoBehaviour
 {
     public GameObject mySphere;
-    public Vector3 myPos;
 
     void Start()
     {
         mySphere.transform.parent = this.transform.parent;
         mySphere.transform.localPosition = new Vector3(0, 0, 0);
-    }
-
-    void Update()
-    {
-        //myPos = gameObject.transform.position;
-        //mySphere.transform.Translate(myPos);
-        //mySphere.transform.Rotate(Quaternion.identity);
-
     }
 }
