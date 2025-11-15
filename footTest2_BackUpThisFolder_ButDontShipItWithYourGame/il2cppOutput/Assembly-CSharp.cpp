8d1af436dbfef8bf116b528530db3b06290d8709
Neeti <neetikumar8@gmail.com>
2025-11-15 07:48:41 +0530

    audioVXF scene fully working
    removed foot vfx and tunnel issue

--- footTest2_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp.cpp
+++ footTest2_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b80cbdae4b66b3ed9c231410e4d5dbae317ac17aae172ff9e0390586a12fa7e
-size 90759
+oid sha256:8838159401d979d5655d7580172a3b5985fbe0eafe77c4fb7c4da9a41bd9acee
+size 90549
