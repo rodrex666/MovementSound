afade9c17936303b263c716ca75dca82b4dec595
Rodrigo <rodrex666@gmail.com>
2025-11-24 19:24:18 +0100

    Sound changes
    Sound Listener FMOD in camera added
    Volume parameters in FMOD project added
    Corrected Sphere 3D Sound for mainMix
    Voice source changed and SphereVoice added, now the voices are also moving
    Movement of hands now are changing the volume of main mix and not more the energy level.

--- Assets/Scripts/FMODPlaywithParameters.cs
+++ Assets/Scripts/FMODPlaywithParameters.cs
@@ -1,23 +1,34 @@
+using FMODUnity;
 using UnityEngine;
 
 public class FMODPlaywithParameters : MonoBehaviour
 {
     FMOD.Studio.EventInstance MainMix;
-    private float _valueFmod =0.5f;
+    private float _valueFmod =1f;
+    public FMODUnity.EventReference instrumentEvent;
+    [SerializeField]
+    private bool _controlParameter = true;
 
     void Start()
     {
-        MainMix = FMODUnity.RuntimeManager.CreateInstance("event:/Main_mix");
+        MainMix= FMODUnity.RuntimeManager.CreateInstance(instrumentEvent);
         MainMix.start();
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(MainMix, gameObject, gameObject.GetComponent<Rigidbody>());
     }
 
     // Update is called once per frame
     void Update()
     {
-        MainMix.setParameterByName("EnergyLevel",_valueFmod);
+        if (_controlParameter)
+        { MainMix.setParameterByName("MainMixVolume", _valueFmod); }
     }
     public void updateParameterFMOD(float value)
     {
         _valueFmod = value;
+    }
+    private void OnDestroy()
+    {
+        MainMix.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        MainMix.release();
     }
 }
