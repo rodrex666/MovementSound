12d58acef76278e14c9fd36f208002340b4d9968
Neeti <neetikumar8@gmail.com>
2025-11-24 17:22:38 +0530

    neater footblastcollision script

--- Assets/Scripts/footBlastCollision.cs
+++ Assets/Scripts/footBlastCollision.cs
@@ -1,42 +1,19 @@
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.VFX;
 
 public class footBlastCollision : MonoBehaviour
 {
-    //public VisualEffect footVFX;
-    //public float currentParticleCount;
-    //public float newParticleCount;
-
     public GameObject explosionHolder;
     public ParticleSystem explodeParticles;
-
     public Vector3 clapPosition;
-
-    public bool hitCollider;
-
 
     void Start()
     {
-        //footVFX = GetComponent<VisualEffect>();
-        hitCollider = false;
-
+        //assign explosion effect
         explodeParticles = explosionHolder.GetComponent<ParticleSystem>();
     }
 
-    void Update()
-    {
-        //if (hitCollider)
-        //{
-        //    currentParticleCount = footVFX.GetFloat("particleCount");
-        //    if (currentParticleCount > 0.1f)
-        //    {
-        //        newParticleCount = currentParticleCount - 0.2f;
-        //        footVFX.SetFloat("particleCount", newParticleCount);
-        //    }
-        //}
-    }
-
+    //move explosion effect to collision location, and play explosion effect on collision with blast collider
     void OnTriggerEnter(Collider other)
     {
         if (other.name == "blastCollider")
@@ -45,7 +22,6 @@ public class footBlastCollision : MonoBehaviour
             explodeParticles.Stop();
             explosionHolder.transform.position = clapPosition;
             explodeParticles.Play();
-            hitCollider = true;
         }
     }
 }
